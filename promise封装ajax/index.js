2639e5b0d33f22aa5db744b19982fac5e81f1636
huangjie <huangjie@xiaohulu.com>
2022-09-26 13:24:03 +0800

    promise封装ajax

---
+++ promise封装ajax/index.js
@@ -0,0 +1,20 @@
+function ajax(url) {
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.responseType = "json";
+    xhr.onload = function () {
+      // 请求完成
+      if (this.status === 200) {
+        resolve(this.response);
+      } else {
+        reject(new Error(this.statusText));
+      }
+    };
+    xhr.send();
+  });
+}
+
+ajax("./data.json").then((data) => {
+  console.log(data);
+});
