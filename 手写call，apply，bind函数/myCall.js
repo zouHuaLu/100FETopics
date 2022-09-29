cd624ed5f51a8706e48e6f5af7bbe01a4f52e41f
zouHuaLu <huangjie1234huang@qq.com>
2022-09-29 17:46:04 +0800

    更新

---
+++ 手写call，apply，bind函数/myCall.js
@@ -0,0 +1,11 @@
+function myCall(context) {
+  if (typeof context !== "undefined" || context === null) {
+    context = window;
+  }
+  let symbol = symbol();
+  context[symbol] = this;
+  let argument = [...arguments].slice(1);
+  let result = context[symbol](...argument);
+  delete context[symbol];
+  return result;
+}
