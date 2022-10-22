8d6c15c5cfc4f5d9625601d3129dc793c82eb0a8
zouHuaLu <huangjie1234huang@qq.com>
2022-10-22 18:45:50 +0800

    vue3收集依赖原理

--- Vue3/reactive/index.js
+++ Vue3/reactive/index.js
@@ -15,7 +15,7 @@ export function reactive(target) {
   const handler = {
     get(target, key, receiver) {
       // 收集依赖
-      console.log("get", key);
+      track(target, key);
       // 如果当前对象的key属性也是对象的话，也需要做响应式
       const result = Reflect.get(target, key, receiver);
       return convert(result);
@@ -26,7 +26,7 @@ export function reactive(target) {
       if (oldValue !== value) {
         result = Reflect.set(target, key, value, receiver);
         // 触发更新
-        console.log("set", key, value);
+        trigger(target, key);
       }
       return result;
     },
@@ -35,11 +35,59 @@ export function reactive(target) {
       const result = Reflect.deleteProperty(target, key);
       if (hadKey && result) {
         // 触发更新
-        console.log("delete", key);
+        trigger(target, key);
       }
       return result;
     },
   };
 
   return new Proxy(target, handler);
+}
+
+/**
+ * 收集依赖
+ * @param {*} target  响应式对象
+ * @param {*} key 响应式对象中的属性
+ *
+ * targetMap：WeakMap实例，key是target对象，value是depsMap
+ * depsMap： map实例，key是target对象中的属性，value是dep
+ * dep：Set实例，存储的是effect中的回调函数
+ */
+
+let activeEffect = null;
+export function effect(callback) {
+  activeEffect = callback;
+  // 收集依赖
+  callback();
+  activeEffect = null;
+}
+
+let targetMap = new WeakMap();
+
+export function track(target, key) {
+  if (!activeEffect) return;
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()));
+  }
+
+  let dep = depsMap.get(key);
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()));
+  }
+  dep.add(activeEffect);
+}
+
+/**
+ * 触发依赖
+ */
+
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) return;
+  let dep = depsMap.get(key);
+  if (!dep) return;
+  dep.forEach((callback) => {
+    callback();
+  });
 }
