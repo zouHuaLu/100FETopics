e92c848d35f2b8805b247e5249c4829561f4dbf1
zouHuaLu <huangjie1234huang@qq.com>
2022-10-26 20:33:05 +0800

    修改

--- 手写new操作符/index.js
+++ 手写new操作符/index.js
@@ -1,6 +1,6 @@
 // 在调用 new 的过程中会发生以上四件事情：
 // （1）首先创建了一个新的空对象
-// （2）设置原型，将对象的原型设置为函数的 prototype 对象。
+// （2）设置原型，将对象的原型__proto__设置为函数的 prototype 对象。
 // （3）让函数的 this 指向这个对象，执行构造函数的代码（为这个新对象添加属性）
 // （4）判断函数的返回值类型，如果是值类型，返回创建的对象。如果是引用类型，就返回这个引用类型的对象。
 
@@ -19,7 +19,7 @@ function myNew() {
   if (typeof constructor !== "function") {
     return;
   }
-  let newObj = Object.create(constructor.prototype); // 新对象指向构造函数的原型对象上
+  let newObj = Object.create(constructor.prototype); // 新对象的__proto__指向构造函数的原型对象上
   let result = constructor.apply(newObj, arguments); // constructor是构造函数，newObj是新对象，
   // 调用构造函数并且将this指向新对象
   let flag =
